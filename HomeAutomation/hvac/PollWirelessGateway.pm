952cffb40e4e25a38f49eef3f700437295a9986e
Wayne (Round Rock) <w5xd@writelog.com>
2017-02-27 04:31:21 +0000

    add WirelessGateway support

---
+++ HomeAutomation/hvac/PollWirelessGateway.pm
@@ -0,0 +1,98 @@
+# perl class to poll the WirelessGateway node to retrieve
+# any events from WirelessThermometers that it might have in
+# its store-and-forward queue. Once retrieved, delete them
+# from its queue.
+
+package hvac::PollWirelessGateway;
+
+use strict;
+
+sub new {
+    my $class = shift;
+    my $self  = {
+        _vars     => shift,
+        _commPort => shift
+    };
+    my %nodeList;
+    foreach (@_) { $nodeList{$_} = 1; }
+    $self->{_nodeList} =
+      \%nodeList;    #remainder of arguments are nodes to check for eheat
+    bless $self, $class;
+    return $self;
+}
+
+sub poll {
+    my $self = shift;
+    my $log  = $self->{_vars}->{FURNACE_LOG_LOCATION};
+    my $cmd =
+        $ENV{HTTPD_LOCAL_ROOT}
+      . "/../hvac/procWirelessGateway "
+      . $self->{_commPort} . " GET";
+    my $toDelete;
+    my ( $my_reader, $my_writer );
+    my $pid = IPC::Open2::open2( $my_reader, $my_writer, $cmd );
+    $my_writer->autoflush(1);
+    $my_reader->autoflush(1);
+    close $my_writer;
+
+    #format of stdout from procWirelessGateway looks like:
+    #3 2017/02/26 21:13:27  65.62 262 -72 123
+    #4 2017/02/26 21:14:11  71.92 283 -85 80
+    #Found delete: 31
+    while ( my $line = <$my_reader> ) {
+        if ( $line =~ /^Found delete:\s/ ) {
+            $toDelete = substr( $line, 14 );
+	    #if we see this line, don't read anymore 
+	    #AND use it to send a DEL command back to the 
+	    #WirelessGateway (below)
+            last;
+        }
+        else {
+            my @splitLine = split( ' ', $line );
+            if ( scalar @splitLine == 7 ) {
+                my $nodeId = shift @splitLine;
+
+                #did INI file say this is an outside temperature?
+                my %recover = %{ $self->{_nodeList} };
+                if ( exists( $recover{$nodeId} ) ) {
+                    $self->{_lastTemperature} = $splitLine[2];
+                }
+
+                #append processed result to temperature file
+                $line = "";
+                foreach (@splitLine) { $line .= $_ . ' '; }
+                my $fn = $self->{_vars}->{FURNACE_LOG_LOCATION}
+                  . "/wirelessThermostat$nodeId.log";
+                open( my $fh, ">>", $fn );
+                print $fh $line . "\n";
+                close $fh;
+            }
+            else {
+                print STDERR "bad line from Gateway: " . $line . "\n";
+            }
+        }
+    }
+    close $my_reader;
+    waitpid( $pid, 0 );
+
+    if (defined $toDelete)
+    {
+      my $cmd =
+        $ENV{HTTPD_LOCAL_ROOT}
+       . "/../hvac/procWirelessGateway "
+       . $self->{_commPort} . " DEL $toDelete";
+      system $cmd;
+    }
+
+    return 1;
+}
+
+sub notifyTemperature {
+    my $self = shift;
+    my $who  = shift;
+    return $who->temperatureEvent( $self->{_lastTemperature},
+        "WIRELESSGATEWAY" );
+}
+
+1;
+
---
+++ HomeAutomation/hvac/procWirelessGateway.cpp
@@ -0,0 +1,397 @@
+// procWirelessGateway.cpp : Defines the entry point for the console application.
+//
+
+#include <time.h> // old unix-style time
+
+#if defined (WIN32)
+#include <w5xdInsteon/impl/PlmMonitorWin.h>
+#elif defined (LINUX32)
+#include <w5xdInsteon/impl/PlmMonitorLinux.h>
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <chrono>
+#include <thread>
+#include <memory>
+#include <iomanip>
+#include <cstring>
+
+/* Program to open a serial link to the WirelessGateway.
+** The COM port name must be on the command line.
+**
+** Two modes to run:
+** We send the GetMessages command to the gateway.
+** Search for the stored messages from WirelessThermometer and append
+** results for each nodeId. We compute a file name for each
+** as specified by prefix and suffix on the command line.
+**
+** OR
+** delete all the messages we processed from the store-and-forward
+** queue in the WirelessGateway.
+*/
+static void GetMessages(w5xdInsteon::PlmMonitorIO &modem);
+static void DeleteFromGateway(w5xdInsteon::PlmMonitorIO &modem, unsigned oldestMessageId);
+
+int main(int argc, char* argv[])
+{
+    unsigned oldestToDelete(0);
+    bool doDelete(false);
+    bool doGet(false);
+    if (argc > 2)
+    {
+        if (strcmp("GET", argv[2]) == 0)
+        {
+            if (argc == 3)
+                doGet = true;
+        }
+        else if (strcmp("DEL", argv[2]) == 0)
+        {
+            if (argc == 4)
+            {
+                doDelete = true;
+                oldestToDelete = atoi(argv[3]);
+            }
+        }
+    }
+    if (!doGet && !doDelete)
+    {
+        std::cerr << "Usage: procWirelessGateway <COMPORT> GET | DEL <N>" << std::endl;
+        return 1;
+    }
+    std::string comport;
+    comport = argv[1];
+
+    w5xdInsteon::PlmMonitorIO modem(comport.c_str(), 9600);
+    int r = modem.OpenCommPort();
+    if (r != 0)
+    {
+        std::cerr << "Failed to open COM port " << comport << " result: " << r << std::endl;
+        return 1;
+    }
+
+    if (doGet)
+        GetMessages(modem);
+    else if (doDelete)
+        DeleteFromGateway(modem, oldestToDelete);
+    return 0;
+}
+// parsing function helper. search for unsigned integer in decimal, followed by space char
+static bool parseForUnsigned(char c, unsigned &target, unsigned &counter, bool &error)
+{
+    if (isdigit(c))
+    {
+        target *= 10;
+        target += c - '0';
+        counter += 1;
+    }
+    else
+    {
+        if (!counter || c != ' ')
+            error = true;
+        else
+            counter = 0;
+        return true;
+    }
+    return false;
+}
+
+// parsing function for fixed string. Search for exactly the string provided
+static bool parseForString(char c, const char *Text, size_t TextSize, unsigned &counter, bool &error)
+{
+    if (c == Text[counter])
+    {
+        if (counter == TextSize - 2)
+        {
+            counter = 0;
+            return true;
+        }
+        else
+            counter++;
+    }
+    else
+        error = true;
+    return false;
+}
+
+static const size_t BUFSIZE = 1024;
+
+static void GetMessages(w5xdInsteon::PlmMonitorIO &modem)
+{
+    // send the command to the WirelessGateway
+    static const char GETMESSAGES[] = "GetMessages\r";
+    modem.Write((unsigned char *)GETMESSAGES, sizeof(GETMESSAGES) - 1);
+
+    // read its answers until we go a time with no answer
+    std::vector<std::string> results;
+    std::unique_ptr<unsigned char[]> buf(new unsigned char[BUFSIZE]);
+    std::string partial;
+    int j = 0;
+    for (;;)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        unsigned w;
+        if (!modem.Read(buf.get(), BUFSIZE, &w))
+            break;
+        if (w == 0)
+            break; // timed out--didn't get anything so we're done
+        j++;
+        for (unsigned i = 0; i < w; i++)
+        {
+            char c = (char)buf[i];
+            if ((c == '\r') || (c == '\n'))
+            {
+                if (!partial.empty())
+                    results.push_back(partial);
+                partial.clear();
+            }
+            else
+                partial += c;
+        }
+    }
+
+    /* GetMessages responds like this:
+          Gateway messad id
+          Seconds ago it logged into the gateway
+          RECeived message from network
+          RSSI of received message
+          NodeId received from
+          Wireless Thermometer internal count
+          Wireless Thermomenter battery indicator
+          Temperature in C
+          QueueBegin
+          Queue 33 5299 REC -59 3 C:44, B:263, T:+20.31
+          Queue 34 5287 REC -28 4 C:22, B:282, T:+21.18
+          Queue 35 5027 REC -59 3 C:45, B:264, T:+20.37
+          Queue 36 4916 REC -28 4 C:23, B:282, T:+21.18
+          Queue 37 4755 REC -58 3 C:46, B:264, T:+20.56
+          QueueEnd
+          QueueBytesFree 9
+          */
+
+    // unix-style time formatter
+    struct tm *local;
+    time_t now;
+    now = time(NULL);
+
+    // track WirelessGateway message ID to delete after reading
+    // (Gateway has limited memory. We have to tell it we have
+    // retrieved successfully to force it delete.)
+    unsigned oldestMessageId;
+    bool foundEntryToDelete(false);
+
+    enum LineParseState_t {
+        QUEUE, MSGID, AGE, REC, RSSI, NODEID,
+        NODECOUNT1, NODECOUNT2, NODECOUNT3,
+        NODEBATTERY1, NODEBATTERY2, NODEBATTERY3,
+        NODETEMPERATURE1, NODETEMPERATURE2, PARSE_SUCCESS
+    };
+
+    // for every line in the gateway response..
+    for (auto &line : results)
+    {
+        // looking for WirelessThermometer reports in the gateway.
+        // They are of this form:
+        //Queue 33 5299 REC -59 3 C:44, B:263, T:+20.31
+
+        LineParseState_t state(QUEUE);
+        unsigned counter(0); // count characters in the current state
+
+        // stuff in the message we look for
+        unsigned messageId(0);
+        unsigned nodeId(0);
+        unsigned nodeCount(0);
+        unsigned nodeBattery(0);
+        unsigned age(0);
+        unsigned rssi(0);
+        bool negRssi(false);
+        float tempC(0);
+
+        unsigned lineIdx(0); // count characters in the line
+
+        for (auto const &c : line)
+        {
+            // delimiters inside a message from WirelessThermometer
+            static const char QText[] = "Queue ";
+            static const char RText[] = "REC ";
+            static const char CText[] = "C:";
+            static const char BText[] = "B:";
+            static const char TText[] = "T:";
+
+            bool error(false);// error flag for this character. aborts line processing
+            switch (state)
+            {
+            case QUEUE:
+                if (parseForString(c, QText, sizeof(QText), counter, error))
+                    state = MSGID;
+                break;
+
+            case MSGID:
+                if (parseForUnsigned(c, messageId, counter, error))
+                    state = AGE;
+                break;
+
+            case AGE:
+                if (parseForUnsigned(c, age, counter, error))
+                    state = REC;
+                break;
+
+            case REC:
+                if (parseForString(c, RText, sizeof(RText), counter, error))
+                    state = RSSI;
+                break;
+
+            case RSSI:
+                if (counter == 0 && c == '-')
+                {
+                    negRssi = true;
+                    counter += 1;
+                }
+                else if (parseForUnsigned(c, rssi, counter, error))
+                    state = NODEID;
+                break;
+
+            case NODEID:
+                if (parseForUnsigned(c, nodeId, counter, error))
+                    state = NODECOUNT1;
+
+                break;
+
+            case NODECOUNT1:
+                if (parseForString(c, CText, sizeof(CText), counter, error))
+                    state = NODECOUNT2;
+                break;
+
+            case NODECOUNT2:
+                if (parseForUnsigned(c, nodeCount, counter, error))
+                {
+                    if (error && c == ',')
+                    {
+                        // normal successful completion
+                        error = false;
+                        state = NODECOUNT3;
+                        counter = 0;
+                    }
+                    else
+                        state = NODEBATTERY1;
+                }
+                break;
+
+            case NODECOUNT3:
+                if (c == ' ')
+                    state = NODEBATTERY1;
+                else
+                    error = true;
+                break;
+
+            case NODEBATTERY1:
+                if (parseForString(c, BText, sizeof(BText), counter, error))
+                    state = NODEBATTERY2;
+                break;
+
+            case NODEBATTERY2:
+                if (parseForUnsigned(c, nodeBattery, counter, error))
+                {
+                    if (error && c == ',')
+                    {
+                        // normal trailing comma
+                        error = false;
+                        state = NODEBATTERY3;
+                        counter = 0;
+                    }
+                    else
+                        state = NODETEMPERATURE1;
+                }
+                break;
+
+            case NODEBATTERY3:
+                if (c == ' ')
+                    state = NODETEMPERATURE1;
+                else
+                    error = true;
+                break;
+
+            case NODETEMPERATURE1:
+                if (parseForString(c, TText, sizeof(TText), counter, error))
+                    state = NODETEMPERATURE2;
+                break;
+
+            case NODETEMPERATURE2:
+            {
+                std::string tempCstr = line.substr(lineIdx);
+                if (!tempCstr.empty() && (tempCstr[0] == '-' || tempCstr[0] == '+'))
+                {
+                    tempC = (float)(atof(tempCstr.c_str()));
+                    state = PARSE_SUCCESS;
+                }
+            }
+            break;
+
+            }
+            lineIdx += 1;
+
+            if (error)
+                break;
+
+            // if we parsed OK on the current character. 
+            // Deal with success, and break the for(:line) loop 
+            if (state == PARSE_SUCCESS)
+            {
+                foundEntryToDelete = true;
+                oldestMessageId = messageId;
+
+                time_t thisEvent = now - age; // account for time inside gateway
+                local = localtime(&thisEvent);
+                char buf[64];
+                // old unix-style time string for first two columns in log
+                sprintf(buf, "%04d/%02d/%02d %02d:%02d:%02d",
+                    local->tm_year + 1900,
+                    local->tm_mon + 1,
+                    local->tm_mday,
+                    local->tm_hour,
+                    local->tm_min,
+                    local->tm_sec);
+
+                 short rssiVal = negRssi ? -(short)rssi : rssi;
+                std::cout << nodeId << " " << buf << " " << std::fixed << std::setw(6) << std::setprecision(2) << tempC / 5.f * 9.f + 32.f << " " <<
+                    nodeBattery << " " <<
+                    rssiVal << " " <<
+                    nodeCount << std::endl;
+                break;  // there are more characters on the line. tempC already got them
+            }
+        }
+    }
+    if (foundEntryToDelete)
+        std::cout << "Found delete: " << oldestMessageId << std::endl;
+    else
+        std::cout << "None found for delete" << std::endl;
+    std::cout << std::flush;
+}
+
+static void DeleteFromGateway(w5xdInsteon::PlmMonitorIO &modem, unsigned oldestMessageId)
+{
+    // Tell WirelessGateway we processed the messages and we won't see them in a future run
+    std::ostringstream msg;
+    msg << "DeleteMessagesFromId " << oldestMessageId << "\r";
+    modem.Write(const_cast<unsigned char *>(reinterpret_cast<const unsigned char *>(msg.str().c_str())), msg.str().length());
+    // a bit for modem to respond
+    std::string response;
+    std::unique_ptr<unsigned char[]> buf(new unsigned char[BUFSIZE]);
+    for (;;)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        unsigned w;
+       if (!modem.Read(buf.get(), BUFSIZE, &w))
+            break;
+        if (w == 0)
+            break; // timed out--didn't get anything so we're done  
+        for (unsigned i = 0; i < w; i++)
+            response += buf[i];
+    }
+    if (!response.empty())
+        std::cerr << "Modem" << response << std::endl;
+}
